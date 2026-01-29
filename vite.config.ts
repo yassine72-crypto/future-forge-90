593dc2eeb5cbc59f40679d8439c7ef311f8c446a
Yassine Aameur <yassine72-crypto@gmail.com>
2026-01-29 14:34:24 +0100

    Build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/future-forge-90/",  // ← هذا السطر خاص GitHub Pages
   server: {
     host: "::",
     port: 8080,
